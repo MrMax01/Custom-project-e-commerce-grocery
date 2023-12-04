d5d9603ffdff81f19ed766d8630531de31cdfc97
MrMax01 <massimo.mauro00@gmail.com>
2023-12-04 17:33:09 +0100

    init commit

---
+++ src/main/java/massimomauro/Customprojectecommercegrocery/CustomProjectECommerceGroceryApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.Customprojectecommercegrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProjectECommerceGroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProjectECommerceGroceryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/Customprojectecommercegrocery/CustomProjectECommerceGroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.Customprojectecommercegrocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProjectECommerceGroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
